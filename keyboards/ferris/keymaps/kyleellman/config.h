a4a0672df4611fd160963c9e88796507fe085472
Kyle Ellman <kyle.ellman@gmail.com>
2022-09-06 13:04:54 -0700

    enable double-shift capsword

--- keyboards/ferris/keymaps/kyleellman/config.h
+++ keyboards/ferris/keymaps/kyleellman/config.h
@@ -38,6 +38,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 
+// CAPS Word
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_ANIMATIONS
